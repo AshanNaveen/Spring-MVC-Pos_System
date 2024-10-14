b0d9009966e223ae8ed6f61d6030db5c6b075681
Naveen Theekshana <ashannvn@gmail.com>
2024-10-14 18:43:40 +0530

    Exceptions are update with no args constructor

--- src/main/java/lk/ijse/sprinpos/exceptions/OrderNotFoundException.java
+++ src/main/java/lk/ijse/sprinpos/exceptions/OrderNotFoundException.java
@@ -1,10 +1,13 @@
 package lk.ijse.sprinpos.exceptions;
 
+import lombok.NoArgsConstructor;
+
 /**
  * @author Naveen Theekshana
  * @date 10/13/2024
  * @project SpringPos
  */
+@NoArgsConstructor
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(String message) {
         super(message);
