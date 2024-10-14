28e4e11dda9eef37e075c16b9113c2223af57e9b
Naveen Theekshana <ashannvn@gmail.com>
2024-10-14 18:42:26 +0530

    dtos are updated with constructors

--- src/main/java/lk/ijse/sprinpos/dto/OrderDetailDTO.java
+++ src/main/java/lk/ijse/sprinpos/dto/OrderDetailDTO.java
@@ -1,11 +1,18 @@
 package lk.ijse.sprinpos.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Naveen Theekshana
  * @date 10/12/2024
  * @project SpringPos
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class OrderDetailDTO {
     private int qtyOnOrder;
     private String itemId;
