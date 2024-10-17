f6a31dfdb846097da4dbb51b1234c62cca41a35f
Naveen Theekshana <ashannvn@gmail.com>
2024-10-17 20:42:00 +0530

    customer controller enhanced to make loggings

--- src/main/java/lk/ijse/sprinpos/controller/CustomerController.java
+++ src/main/java/lk/ijse/sprinpos/controller/CustomerController.java
@@ -1,12 +1,15 @@
 package lk.ijse.sprinpos.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lk.ijse.sprinpos.customObj.CustomerErrorResponse;
-import lk.ijse.sprinpos.customObj.CustomerResponce;
+import lk.ijse.sprinpos.customObj.CustomerResponse;
 import lk.ijse.sprinpos.dto.impl.CustomerDTO;
 import lk.ijse.sprinpos.exceptions.CustomerNotFoundException;
 import lk.ijse.sprinpos.exceptions.DataPersistFailedException;
 import lk.ijse.sprinpos.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,26 +26,30 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("api/v1/customer")
+@RequiredArgsConstructor
 public class CustomerController {
 
-    @Autowired
     private CustomerService customerService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     @GetMapping(value = "/{custId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public CustomerResponce getCustomer(@PathVariable("custId") String custId) {
+    public CustomerResponse getCustomer(@PathVariable("custId") String custId, HttpServletRequest request) {
         CustomerDTO customer = customerService.getSelectedCustomer(custId);
+        logger.info("Received request to get customer on " + request.getRemoteAddr());
         return customer == null ? new CustomerErrorResponse() : customer;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CustomerDTO> getAllCustomers() {
+    public List<CustomerDTO> getAllCustomers(HttpServletRequest request) {
+        logger.info("Received request to get all customers on " + request.getRemoteAddr());
         return customerService.getAllCustomers();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createCustomer(@RequestBody CustomerDTO dto) {
+    public ResponseEntity<Void> createCustomer(@RequestBody CustomerDTO dto, HttpServletRequest request) {
         try {
             customerService.saveCustomer(dto);
+            logger.info("Received request to save customer on " + request.getRemoteAddr());
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (DataPersistFailedException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -53,9 +60,10 @@ public class CustomerController {
     }
 
     @PutMapping(value = "/{custId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> updateCustomer(@PathVariable("custId") String custId, @RequestBody CustomerDTO dto) {
+    public ResponseEntity<String> updateCustomer(@PathVariable("custId") String custId, @RequestBody CustomerDTO dto, HttpServletRequest request) {
         try {
             customerService.updateCustomer(custId, dto);
+            logger.info("Received request to update customer on " + request.getRemoteAddr());
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (CustomerNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -65,9 +73,10 @@ public class CustomerController {
     }
 
     @DeleteMapping(value = "/{custId}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable("custId") String custId) {
+    public ResponseEntity<String> deleteCustomer(@PathVariable("custId") String custId, HttpServletRequest request) {
         try {
             customerService.deleteCustomer(custId);
+            logger.info("Received request to delete customer on " + request.getRemoteAddr());
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (CustomerNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
