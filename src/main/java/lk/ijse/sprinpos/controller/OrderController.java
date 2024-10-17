1b2a75ed87f30d65148e00d78a7fbe3a50308617
Naveen Theekshana <ashannvn@gmail.com>
2024-10-17 20:42:15 +0530

    order controller enhanced to make loggings

--- src/main/java/lk/ijse/sprinpos/controller/OrderController.java
+++ src/main/java/lk/ijse/sprinpos/controller/OrderController.java
@@ -1,9 +1,18 @@
 package lk.ijse.sprinpos.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lk.ijse.sprinpos.customObj.OrderErrorResponse;
+import lk.ijse.sprinpos.customObj.OrderResponse;
 import lk.ijse.sprinpos.dto.impl.OrderDTO;
+import lk.ijse.sprinpos.exceptions.CustomerNotFoundException;
+import lk.ijse.sprinpos.exceptions.DataPersistFailedException;
 import lk.ijse.sprinpos.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,33 +25,62 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("api/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
-    @Autowired
     private OrderService orderService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     @GetMapping(value = "/{orderId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public OrderDTO getOrder(@PathVariable("orderId") String orderId) {
-        return orderService.getSelectedOrder(orderId);
+    public OrderResponse getOrder(@PathVariable("orderId") String orderId, HttpServletRequest request) {
+        OrderDTO order = orderService.getSelectedOrder(orderId);
+        logger.info("Received request to get order on " + request.getRemoteAddr());
+        return order == null ? new OrderErrorResponse() : order;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<OrderDTO> getAllOrders() {
+    public List<OrderDTO> getAllOrders(HttpServletRequest request) {
+        logger.info("Received request to get all orders on " + request.getRemoteAddr());
         return orderService.getAllOrders();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestBody OrderDTO order) {
-        orderService.saveOrder(order);
+    public ResponseEntity<Void> createOrder(@RequestBody OrderDTO order, HttpServletRequest request) {
+        try {
+            orderService.saveOrder(order);
+            logger.info("Received request to save order on " + request.getRemoteAddr());
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistFailedException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping(value = "/{orderId}",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void updateOrder(@PathVariable("orderId") String orderId, @RequestBody OrderDTO orderDTO) {
-        orderService.updateOrder(orderId, orderDTO);
+    @PutMapping(value = "/{orderId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateOrder(@PathVariable("orderId") String orderId, @RequestBody OrderDTO orderDTO, HttpServletRequest request) {
+        try {
+            orderService.updateOrder(orderId, orderDTO);
+            logger.info("Received request to update order on " + request.getRemoteAddr());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping(value = "/{orderId}")
-    public void deleteOrder(@PathVariable("orderId") String orderId) {
-        orderService.deleteOrder(orderId);
+    public ResponseEntity<Void> deleteOrder(@PathVariable("orderId") String orderId , HttpServletRequest request) {
+        try {
+            orderService.deleteOrder(orderId);
+            logger.info("Received request to delete order on " + request.getRemoteAddr());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
